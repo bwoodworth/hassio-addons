0fd0250ffe333bfcdccdf2e49621bcb0bf037694
Brian Woodworth <bwoodworth@gmail.com>
2020-02-25 14:05:41 -0800

    changed circuits to be dynamic

--- pentair-screenlogic/initialize.js
+++ pentair-screenlogic/initialize.js
@@ -42,36 +42,14 @@ function connect(client) {
     console.log(' controllerId=' + config.controllerId);
     console.log(' pumpCircArray=' + config.pumpCircArray);
     console.log(' Number of bodyArray Objects=' + config.bodyArray.length);
-// I'm sure there's a better way to do this, but I don't know what it is. You need a [#]  (0 to x) for each bodyArray object that is programed in your system. The count of bodyArray objects is obtained by the previous line of code. Run $ node ./example.js to see the result.
     console.log(' bodyArray=');
-	console.log('   circuitId: ' + config.bodyArray[0].circuitId);
-	console.log('     name: ' + config.bodyArray[0].name);
-	console.log('   circuitId: ' + config.bodyArray[1].circuitId);
-	console.log('     name: ' + config.bodyArray[1].name);
-	console.log('   circuitId: ' + config.bodyArray[2].circuitId);
-	console.log('     name: ' + config.bodyArray[2].name);
-	console.log('   circuitId: ' + config.bodyArray[3].circuitId);
-	console.log('     name: ' + config.bodyArray[3].name);
-	console.log('   circuitId: ' + config.bodyArray[4].circuitId);
-	console.log('     name: ' + config.bodyArray[4].name);
-	console.log('   circuitId: ' + config.bodyArray[5].circuitId);
-	console.log('     name: ' + config.bodyArray[5].name);
-	console.log('   circuitId: ' + config.bodyArray[6].circuitId);
-	console.log('     name: ' + config.bodyArray[6].name);
-	console.log('   circuitId: ' + config.bodyArray[7].circuitId);
-	console.log('     name: ' + config.bodyArray[7].name);
-	console.log('   circuitId: ' + config.bodyArray[8].circuitId);
-	console.log('     name: ' + config.bodyArray[8].name);
-	console.log('   circuitId: ' + config.bodyArray[9].circuitId);
-	console.log('     name: ' + config.bodyArray[9].name);
-	console.log('   circuitId: ' + config.bodyArray[10].circuitId);
-	console.log('     name: ' + config.bodyArray[10].name);
-	console.log('   circuitId: ' + config.bodyArray[11].circuitId);
-	console.log('     name: ' + config.bodyArray[11].name);
-	console.log('   circuitId: ' + config.bodyArray[12].circuitId);
-	console.log('     name: ' + config.bodyArray[12].name);
-
-    client.close();
+    var i;
+    for(i = 0; i < config.bodyArray.length; I++)
+    {
+      console.log('   circuitId: ' + config.bodyArray[i].circuitId);
+	    console.log('     name: ' + config.bodyArray[i].name);
+    }
+	  client.close();
   });
 
   client.connect();
--- pentair-screenlogic/send_state_to_ha.js
+++ pentair-screenlogic/send_state_to_ha.js
@@ -19,20 +19,11 @@ function connect(client) {
     console.log('pentair/saltppm/state,' + status.saltPPM);
     console.log('pentair/ph/state,' + status.pH);
     console.log('pentair/saturation/state,' + status.saturation);
-    console.log('pentair/circuit/' + status.circuitArray[0].id + '/state,' + (status.circuitArray[0].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[1].id + '/state,' + (status.circuitArray[1].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[2].id + '/state,' + (status.circuitArray[2].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[3].id + '/state,' + (status.circuitArray[3].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[4].id + '/state,' + (status.circuitArray[4].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[5].id + '/state,' + (status.circuitArray[5].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[6].id + '/state,' + (status.circuitArray[6].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[7].id + '/state,' + (status.circuitArray[7].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[8].id + '/state,' + (status.circuitArray[8].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[9].id + '/state,' + (status.circuitArray[9].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[10].id + '/state,' + (status.circuitArray[10].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[11].id + '/state,' + (status.circuitArray[11].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[12].id + '/state,' + (status.circuitArray[12].state ? 'ON' : 'OFF'));
-    console.log('pentair/circuit/' + status.circuitArray[13].id + '/state,' + (status.circuitArray[13].state ? 'ON' : 'OFF'));
+    var i;
+    for(i = 0; i < config.bodyArray.length; I++)
+    {
+      console.log('pentair/circuit/' + status.circuitArray[i].id + '/state,' + (status.circuitArray[i].state ? 'ON' : 'OFF'));
+    }
   }).on('chemicalData', function(chemData) {
     console.log('pentair/calcium/state,' + chemData.calcium);
     console.log('pentair/cyanuricacid/state,' + chemData.cyanuricAcid);
